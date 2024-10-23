3817a303669efef3089d6e105b4f1e040b45f6da
doitidey <evxrafter@gmail.com>
2024-10-23 23:41:26 +0900

    Feature: useProduct 업데이트 구현

--- src/refactoring/hooks/useProduct.ts
+++ src/refactoring/hooks/useProduct.ts
@@ -1,6 +1,21 @@
-import { useState } from 'react';
-import { Product } from '../../types.ts';
+import { useState } from "react";
+import { Product } from "../../types.ts";
 
 export const useProducts = (initialProducts: Product[]) => {
-  return { products: [], updateProduct: () => undefined, addProduct: () => undefined };
+  const [products, setProducts] = useState<Product[]>(initialProducts);
+
+  const updateProduct = (updatedProduct: Product) => {
+    setProducts((prevProducts) => replaceProduct(prevProducts, updatedProduct));
+  };
+
+  function replaceProduct(
+    products: Product[],
+    updatedProduct: Product
+  ): Product[] {
+    return products.map((product) =>
+      product.id === updatedProduct.id ? updatedProduct : product
+    );
+  }
+
+  return { products, updateProduct, addProduct: () => undefined };
 };
