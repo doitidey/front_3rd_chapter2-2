714365fc4885de3b48482b648caa8aeaab6d747b
doitidey <evxrafter@gmail.com>
2024-10-25 00:25:28 +0900

    Fix: updateCartItemQuantity

--- src/refactoring/hooks/utils/cartUtils.ts
+++ src/refactoring/hooks/utils/cartUtils.ts
@@ -1,68 +1,80 @@
-import { CartItem, Coupon } from '../../../types';
+import { CartItem, Coupon } from "../../../types";
 
 export const calculateItemTotal = (item: CartItem) => {
-	const discountRate = getMaxApplicableDiscount(item);
-	return getPriceBeforeDiscount(item) * (1 - discountRate);
+  const discountRate = getMaxApplicableDiscount(item);
+  return getPriceBeforeDiscount(item) * (1 - discountRate);
 };
 
 const getPriceBeforeDiscount = (item: CartItem) => {
-	const { product, quantity } = item;
-	return product.price * quantity;
+  const { product, quantity } = item;
+  return product.price * quantity;
 };
 
 export const getMaxApplicableDiscount = (item: CartItem) => {
-	const { product, quantity } = item;
+  const { product, quantity } = item;
 
-	const maxDiscount = product.discounts
-		.filter((discount) => quantity >= discount.quantity)
-		.reduce((maxRate, discount) => Math.max(maxRate, discount.rate), 0);
+  const maxDiscount = product.discounts
+    .filter((discount) => quantity >= discount.quantity)
+    .reduce((maxRate, discount) => Math.max(maxRate, discount.rate), 0);
 
-	return maxDiscount;
+  return maxDiscount;
 };
 
-export const calculateCartTotal = (cart: CartItem[], selectedCoupon: Coupon | null) => {
-	const totalBeforeDiscount = cart
-		.map((item) => getPriceBeforeDiscount(item))
-		.reduce((prev, cur) => {
-			return (prev += cur);
-		}, 0);
+export const calculateCartTotal = (
+  cart: CartItem[],
+  selectedCoupon: Coupon | null
+) => {
+  const totalBeforeDiscount = cart
+    .map((item) => getPriceBeforeDiscount(item))
+    .reduce((prev, cur) => {
+      return (prev += cur);
+    }, 0);
 
-	let totalAfterDiscount = cart
-		.map((item) => calculateItemTotal(item))
-		.reduce((prev, cur) => {
-			return (prev += cur);
-		}, 0);
+  let totalAfterDiscount = cart
+    .map((item) => calculateItemTotal(item))
+    .reduce((prev, cur) => {
+      return (prev += cur);
+    }, 0);
 
-	if (selectedCoupon !== null) {
-		if (selectedCoupon.discountType === 'amount') {
-			totalAfterDiscount = Math.max(0, totalAfterDiscount - selectedCoupon.discountValue);
-		} else {
-			totalAfterDiscount *= 1 - selectedCoupon.discountValue / 100;
-		}
-	}
+  if (selectedCoupon !== null) {
+    if (selectedCoupon.discountType === "amount") {
+      totalAfterDiscount = Math.max(
+        0,
+        totalAfterDiscount - selectedCoupon.discountValue
+      );
+    } else {
+      totalAfterDiscount *= 1 - selectedCoupon.discountValue / 100;
+    }
+  }
 
-	const totalDiscount = totalBeforeDiscount - totalAfterDiscount;
+  const totalDiscount = totalBeforeDiscount - totalAfterDiscount;
 
-	return {
-		totalBeforeDiscount,
-		totalAfterDiscount,
-		totalDiscount,
-	};
+  return {
+    totalBeforeDiscount,
+    totalAfterDiscount,
+    totalDiscount,
+  };
 };
 
 const updateProductQuantity = (item: CartItem, newQuantity: number) => {
-	const quantityLimit = item.product.stock;
-	const quantity = quantityLimit < newQuantity ? quantityLimit : newQuantity;
-	return { ...item, quantity: quantity };
+  const quantityLimit = item.product.stock;
+  const quantity = quantityLimit < newQuantity ? quantityLimit : newQuantity;
+  return { ...item, quantity: quantity };
 };
 
-export const updateCartItemQuantity = (cart: CartItem[], productId: string, newQuantity: number): CartItem[] => {
-	if (newQuantity === 0) {
-		const cartItemList = cart.filter((item) => item.product.id !== productId);
-		return cartItemList;
-	}
-	const cartItemList = cart.map((item) =>
-		item.product.id === productId ? updateProductQuantity(item, newQuantity) : item
-	);
-	return cartItemList;
+export const updateCartItemQuantity = (
+  cart: CartItem[],
+  productId: string,
+  newQuantity: number
+): CartItem[] => {
+  if (newQuantity > 0) {
+    const cartItemList = cart.map((item) =>
+      item.product.id === productId
+        ? updateProductQuantity(item, newQuantity)
+        : item
+    );
+    return cartItemList;
+  }
+  const cartItemList = cart.filter((item) => item.product.id !== productId);
+  return cartItemList;
 };
